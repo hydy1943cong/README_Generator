05df6ca07ff87ccf1f538ca31915ab2b7a064c35
Cong Wang <wangcong525@hotmail.com>
2024-07-29 00:18:42 -0400

    input questions added

--- Develop/index.js
+++ Develop/index.js
@@ -4,48 +4,59 @@ const colors = require("colors");
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-// const questions = [
-//     {
-//         type: 'input',
-//         name: 'name',
-//         message: colors.brightMagenta("What is your name?")
-//     },
-//     {
-//         type: 'input',
-//         name: 'location',
-//         message: colors.brightMagenta("What is your location?")
-//     },    
-//     {
-//         type: 'input',
-//         name: 'bio',
-//         message: colors.brightMagenta("Provide a brief description of your background and/or experiences")
-//     },
-//     {
-//         type: 'checkbox',
-//         message: colors.brightMagenta('What languages do you know?'),
-//         name: 'languages',
-//         choices: ['html', 'javascript', 'css','java']
-//     },
-//     {
-//         type: 'input',
-//         name: 'linkedin',
-//         message: colors.brightMagenta("Provide your LinkedIn URL")
-//     },
-//     {
-//         type: 'input',
-//         name: 'github',
-//         message: colors.brightMagenta("Provide your GitHub URL")
-//     },
-//     {
-//         type: 'input',
-//         name: 'imgURL',
-//         message: colors.brightMagenta("Provide your background image URL")
-//     }
-// ]
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: colors.brightMagenta("What is the title of your project?")
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: colors.brightMagenta("Provide a brief description of your project.")
+    },
+    {
+        type: 'input',
+        message: colors.brightMagenta('Pleae provide the installation instructions of your project.'),
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: colors.brightMagenta("Provide usage information.")
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: colors.brightMagenta("Who are the contributors?")
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: colors.brightMagenta("Please provide test instructions if there is any.")
+    },
+    {
+        type: 'checkbox',
+        message: colors.brightMagenta('Please choose a license for you application.'),
+        name: 'license',
+        choices: ['None', 'MIT License', 'Apache License 2.0','GNU General Public License v3.0','BSD 2-clause "Simplified" license','BSD 3-clause "New" or "Revised" license']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: colors.brightMagenta("Please provide your the user name of your github.")
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: colors.brightMagenta("Please provide your email address if you'd like to be contacted.")
+    }
+
+]
 
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 
 // // TODO: Create a function to initialize app
 // function init() {}
