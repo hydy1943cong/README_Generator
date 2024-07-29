83bd05bc5b03c5a60dec2208074f235612b911f3
Cong Wang <wangcong525@hotmail.com>
2024-07-28 23:43:56 -0400

    install colors and inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,54 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const colors = require("colors");
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [
+//     {
+//         type: 'input',
+//         name: 'name',
+//         message: colors.brightMagenta("What is your name?")
+//     },
+//     {
+//         type: 'input',
+//         name: 'location',
+//         message: colors.brightMagenta("What is your location?")
+//     },    
+//     {
+//         type: 'input',
+//         name: 'bio',
+//         message: colors.brightMagenta("Provide a brief description of your background and/or experiences")
+//     },
+//     {
+//         type: 'checkbox',
+//         message: colors.brightMagenta('What languages do you know?'),
+//         name: 'languages',
+//         choices: ['html', 'javascript', 'css','java']
+//     },
+//     {
+//         type: 'input',
+//         name: 'linkedin',
+//         message: colors.brightMagenta("Provide your LinkedIn URL")
+//     },
+//     {
+//         type: 'input',
+//         name: 'github',
+//         message: colors.brightMagenta("Provide your GitHub URL")
+//     },
+//     {
+//         type: 'input',
+//         name: 'imgURL',
+//         message: colors.brightMagenta("Provide your background image URL")
+//     }
+// ]
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
